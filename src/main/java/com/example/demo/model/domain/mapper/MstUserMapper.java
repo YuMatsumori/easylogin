fb8b3d7856500e5b1f2857c82b2f69dca02f7c0c
松森　悠 <matsumoriyu1998@yahoo.ne.jp>
2025-05-01 10:54:26 +0900

    easylogin

---
+++ src/main/java/com/example/demo/model/domain/mapper/MstUserMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.model.domain.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demo.model.domain.MstUser;
+
+
+@Mapper
+public interface MstUserMapper {
+	
+	@Select("SELECT * FROM mst_user WHERE user_name = #{userName} AND password = #{password}")
+	MstUser findByUserNameAndPassword( String userName, String password );
+	
+	
+	
+	
+}
